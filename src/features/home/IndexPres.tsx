aaf19273f7ae8e4a9f30f35195184c081ade5d32
pay-hayashi <junlaurantlaurant@gmail.com>
2024-01-08 15:49:10 +0900

    add: react-scroll

--- src/features/home/IndexPres.tsx
+++ src/features/home/IndexPres.tsx
@@ -1,77 +1,86 @@
+'use client'
 import {ReactNode} from 'react'
+import {Link} from 'react-scroll'
 import {twMerge} from 'tailwind-merge'
 
-const contents = [{
-  title: 'About me',
-  body: (key) => (
-    <div key={key} className="grid grid-cols-1 gap-5 text-3xl tracking-wider">
-      <p>
-        I am a web engineer based in Tokyo, specializing in front-end development.
-      </p>
-      <p>
-        While my primary focus is on front-end technologies, I also have a skill set that includes back-end
-        development.
-      </p>
-      <p>
-        My expertise encompasses working with TypeScript, Next.js, Java, and Spring Boot.
-      </p>
-      <p>
-        In addition to my professional work, I am keen about generative art and creative coding, creating
-        pieces in my free time.
-      </p>
-    </div>
-  )
-},
-{
-  title: 'Tech stack',
-  body: (key) => (
-    <div key={key} className="flex flex-wrap gap-x-32 gap-y-10">
-      {
-        ([
-          ['Language', ['Javascript', 'Typescript', 'Java', 'Python', 'VB']],
-          ['Framework', ['Next.js', 'Astro', 'Spring Boot', 'PyTorch', 'p5.js']],
-        ] as [string, string[]][]).map(([title, items], i) => (
-          <div key={i} className="text-2xl flex tracking-wider">
-            <h3 className="w-56 text-4xl">{title}</h3>
-            <ul className="flex flex-col gap-y-3">
-              {
-                items.map((item, j) => (
-                  <li key={j}>{item}</li>
-                ))
-              }
-            </ul>
-          </div>
-        ))
-      }
-    </div>
-  )
-},
-{
-  title: 'Blog',
-  body: (key) => (
-    <div key={key} className="text-2xl">
-      <p>Coming soon...</p>
-    </div>
-  )
-},
-{
-  title: 'Contact',
-  body: (key) => (
-    <dl key={key} className="flex flex-col gap-5 text-2xl">
-      {
-        ([
-          ['X / Twitter', '@pancake_tor'],
-          ['Email', 'pancake.tor@gmail.com'],
-        ] as [string, string][]).map(([dt, dd], i) => (
-          <div key={i} className="flex">
-            <dt className="w-40 font-bold">{dt}</dt>
-            <dd>{dd}</dd>
-          </div>
-        ))
-      }
-    </dl>
-  )
-}] as { title: string, body: (key: number) => ReactNode }[]
+type Content = { title: string, idName: string, body: (key: number) => ReactNode }
+const contents: Content[] = [
+  {
+    title: 'About me',
+    idName: 'about-me',
+    body: (key) => (
+      <div key={key} className="grid grid-cols-1 gap-5 text-3xl tracking-wider">
+        <p>
+          I am a web engineer based in Tokyo, specializing in front-end development.
+        </p>
+        <p>
+          While my primary focus is on front-end technologies, I also have a skill set that includes back-end
+          development.
+        </p>
+        <p>
+          My expertise encompasses working with TypeScript, Next.js, Java, and Spring Boot.
+        </p>
+        <p>
+          In addition to my professional work, I am keen about generative art and creative coding, creating
+          pieces in my free time.
+        </p>
+      </div>
+    )
+  },
+  {
+    title: 'Tech stack',
+    idName: 'tech-stack',
+    body: (key) => (
+      <div key={key} className="flex flex-wrap gap-x-32 gap-y-10">
+        {
+          ([
+            ['Language', ['Javascript', 'Typescript', 'Java', 'Python', 'VB']],
+            ['Framework', ['Next.js', 'Astro', 'Spring Boot', 'PyTorch', 'p5.js']],
+          ] as [string, string[]][]).map(([title, items], i) => (
+            <div key={i} className="text-2xl flex tracking-wider">
+              <h3 className="w-56 text-4xl">{title}</h3>
+              <ul className="flex flex-col gap-y-3">
+                {
+                  items.map((item, j) => (
+                    <li key={j}>{item}</li>
+                  ))
+                }
+              </ul>
+            </div>
+          ))
+        }
+      </div>
+    )
+  },
+  {
+    title: 'Blog',
+    idName: 'blog',
+    body: (key) => (
+      <div key={key} className="text-2xl">
+        <p>Coming soon...</p>
+      </div>
+    )
+  },
+  {
+    title: 'Contact',
+    idName: 'contact',
+    body: (key) => (
+      <dl key={key} className="flex flex-col gap-5 text-2xl">
+        {
+          ([
+            ['X / Twitter', '@pancake_tor'],
+            ['Email', 'pancake.tor@gmail.com'],
+          ] as [string, string][]).map(([dt, dd], i) => (
+            <div key={i} className="flex">
+              <dt className="w-40 font-bold">{dt}</dt>
+              <dd>{dd}</dd>
+            </div>
+          ))
+        }
+      </dl>
+    )
+  },
+]
 
 const padding = 'px-5 md:px-20'
 export const IndexPres = () => {
@@ -84,14 +93,17 @@ export const IndexPres = () => {
         </div>
         <div className="flex gap-5 font-bold">
           {
-            contents.map(({title}, i) => (
-              <a
+            contents.map(({title, idName}, i) => (
+              <Link
                 key={i}
-                href={`#${i + 1}`}
-                className="hover:underline"
+                to={idName}
+                activeClass={''}
+                spy={true}
+                smooth={true}
+                offset={-100}
               >
                 {title}
-              </a>
+              </Link>
             ))
           }
         </div>
@@ -100,10 +112,10 @@ export const IndexPres = () => {
       {/* Body */}
       <div className={'my-32 grid grid-cols-1 gap-56'}>
         {
-          contents.map(({title, body}, i) => (
+          contents.map(({title, idName, body}, i) => (
             <div
               key={i}
-              id={`${i + 1}`}
+              id={idName}
               className={twMerge(
                 `${padding} flex flex-col gap-10`,
                 `${i % 2 ? 'lg:flex-row-reverse' : 'lg:flex-row'} lg:gap-14`
